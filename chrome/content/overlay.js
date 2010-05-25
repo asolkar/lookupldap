d2988531ca596470ad5cb7b0f540b8a831c4cc5e
Mahesh Asolkar <mahesh@mahesha.com>
2010-05-24 22:42:29 -0700

    Added item to msgHdrEmail popup menu

---
+++ chrome/content/overlay.js
@@ -0,0 +1,16 @@
+var lookupldap = {
+  onLoad: function() {
+    // initialization code
+    this.initialized = true;
+    this.strings = document.getElementById("lookupldap-strings");
+  },
+
+  onMsgHdrEmailCommand: function(e) {
+    var promptService = Components.classes["@mozilla.org/embedcomp/prompt-service;1"]
+                                  .getService(Components.interfaces.nsIPromptService);
+    promptService.alert(window, this.strings.getString("helloMessageTitle"),
+                                this.strings.getString("helloMessage"));
+  }
+};
+
+window.addEventListener("load", function(e) { lookupldap.onLoad(e) }, false);
---
+++ defaults/preferences/prefs.js
@@ -0,0 +1,6 @@
+pref("extensions.lookupldap.boolpref", false);
+pref("extensions.lookupldap.intpref", 0);
+pref("extensions.lookupldap.stringpref", "A string");
+
+// https://developer.mozilla.org/en/Localizing_extension_descriptions
+pref("extensions.lookupldap@mahesh.asolkar.description", "chrome://lookupldap/locale/overlay.properties");
