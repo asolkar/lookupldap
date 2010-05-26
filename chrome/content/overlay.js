331b4b869dcb74b47a416fe070611570216ebe76
Mahesh Asolkar <mahesh@mahesha.com>
2010-05-25 21:08:16 -0700

    Got correct emailAddressNode from popupNode

--- chrome/content/overlay.js
+++ chrome/content/overlay.js
@@ -11,8 +11,18 @@ var lookupldap = {
     // }
   },
 
-  onMsgHdrEmailCommand: function(emailAddressNode) {
-    this.debugMessage ("Email Address Node: " + emailAddressNode);
+  onMsgHdrEmailCommand: function(popupNode) {
+    //
+    // MMA: Is this correct? Isn't there a better way to do this?
+    //      If not, at least do some null checks on Nodes below.
+    //
+
+    //
+    // Get emailAddressNode from popupNode. popupNode points to
+    // the label, we want the actual email address node.
+    //
+    var emailAddressNode = popupNode.parentNode.parentNode;
+
     if (emailAddressNode) {
       var emailAddress = emailAddressNode.getAttribute("emailAddress");
       var displayName = emailAddressNode.getAttribute("displayName");
